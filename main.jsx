1fa0b2d1b3a2482945e9cdf41cffcd77257441f0
Ashwini Pote <68552155+Ashwini1011@users.noreply.github.com>
2022-08-09 19:51:09 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Formpractice_2 from './components/Formpractice_2'
+import './index.css'
+import Form_func from './components/Form_func'
+import Calculator from './components/Calculator'
+import Todo from './components/Todo'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Todo/>
+  </React.StrictMode>
+)
